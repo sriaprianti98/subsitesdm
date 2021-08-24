fed2beabef7cb9d1827d83a6b9194f528c53c7ba
zainikhlas <zainulikhlas32@gmail.com>
2021-08-24 12:36:42 +0700

    perubahan isi migrasi

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# import uuid
 # from datetime import datetime
 import datetime
 from django.utils.crypto import get_random_string
